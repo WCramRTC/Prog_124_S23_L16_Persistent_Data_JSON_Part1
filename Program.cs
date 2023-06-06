60d0e0f8421734e550abc4fe49e3f4efdc3d24d2
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-06-05 18:39:52 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
-﻿namespace Prog_124_S23_L16_Persistent_Data_JSON_Part1
+﻿using System.Text.Json;
+
+namespace Prog_124_S23_L16_Persistent_Data_JSON_Part1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SaveJSON();
         } // Main
+
+        public static void SaveJSON()
+        {
+            JsonSerializerOptions jso = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            string[] names = { "Henry", "Cirella", "Meili" };
+            int[] grades = { 110, 120, 130 };
+
+            string jsonManager = JsonSerializer.Serialize(grades, jso);
+
+            Console.WriteLine(jsonManager);
+
+            //File.WriteAllText(Data.userFilePath, jsonManager);
+        }
+
 
     } // class
 
